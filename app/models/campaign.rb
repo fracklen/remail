fbbc671c1543103529d4145465189f7e2c7ba1f3
Martin Neiiendam <fracklen@gmail.com>
2016-02-17 16:41:43 +0100

    Allow viewing progress of campaign run

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,10 @@ class Campaign < ActiveRecord::Base
   belongs_to :recipient_list
   belongs_to :customer
   belongs_to :domain
+  has_many :campaign_runs
 
   validates :recipient_list, presence: true
   validates :customer, presence: true
   validates :domain, presence: true
+
 end
